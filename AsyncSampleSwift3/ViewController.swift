922a52953cb0089f77869f2b168639b5a267da5f
ohde-sg <ohde@socialgrid.co.jp>
2016-09-18 23:31:30 +0900

    add async button and elseAction button

--- AsyncSampleSwift3/ViewController.swift
+++ AsyncSampleSwift3/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var elseActionLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func touchMyButton(_ sender: UIButton) {
+        self.myLabel.text = "loading...";
+        DispatchQueue.global().async {
+            //something heavy job
+            self.heavyJob()
+            let sum:UInt32 = arc4random_uniform(10)
+            DispatchQueue.main.sync {
+                self.myLabel.text = "result:\(sum)"
+            }
+        }
+    }
+    @IBAction func touchEleActionButton(_ sender: UIButton) {
+        self.elseActionLabel.text = "else: \(arc4random_uniform(10))";
+    }
+    
+    func heavyJob(){
+        sleep(3)
+    }
 
 }
 
