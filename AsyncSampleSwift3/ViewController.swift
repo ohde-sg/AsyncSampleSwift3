09a2a6f983470a8295217d6573cc18da590ca491
ohde-sg <ohde@socialgrid.co.jp>
2016-09-18 22:56:02 +0900

    Initial Commit

---
+++ AsyncSampleSwift3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AsyncSampleSwift3
+//
+//  Created by 大出喜之 on 2016/09/18.
+//  Copyright © 2016年 yoshiyuki ohde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
